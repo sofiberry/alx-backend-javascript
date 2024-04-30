2fad2877005350f85c0b377a5ac527b66f40fd3a
solomon belay <sofi2darling@gmail.com>
2024-04-30 11:28:17 -0700

    make files executable

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdout.wire('welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+
+	const chunk = process.stdin.read();
+
+	if(chunk) {
+		process.stdout.write(`your name is : ${chunk}`);
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write('this important software is no closing\n');
+});
