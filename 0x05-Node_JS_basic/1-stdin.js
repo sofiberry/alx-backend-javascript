c3917a818be122388a90480bcd5ce552285f23fd
solomon belay <sofi2darling@gmail.com>
2024-05-01 06:53:14 -0700

    file updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
@@ -7,16 +6,6 @@ process.stdin.on('readable', () => {
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
-=======
-process.stdout.write('welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('readable', () => {
-	const chunk = process.stdin.read();
-
-	if (chunk) {
-		process.stdout.write(`your name is : ${chunk}`);
-	}
->>>>>>> e0ce7a4ab56cbbcaa97ae880e4ba0e0d8ad0164b
 });
 
 process.stdin.on('end', () => {
