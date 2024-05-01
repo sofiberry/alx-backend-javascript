66fa468f4232df6ad0fba4b11580c9802bdeb1e7
solomon belay <sofi2darling@gmail.com>
2024-05-01 06:29:20 -0700

    file updated

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,14 @@
-const express = require('express'); 
+const express = require('express');
 
 const app = express();
 const PORT = 1245;
 
 app.get('/', (_, res) => {
-	res.send)'Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-	console.log(`server listening on PORT ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
